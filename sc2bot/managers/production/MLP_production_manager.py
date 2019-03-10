72993a7303f3a6f3d13371d49eaa52b29dc52336
Miguel González <miguelgondu@gmail.com>
2019-03-10 16:41:01 +0100

    Starts translation observation-to-row

--- sc2bot/managers/production/MLP_production_manager.py
+++ sc2bot/managers/production/MLP_production_manager.py
@@ -15,33 +15,48 @@ class MLPProductionManager(ProductionManager):
         super().__init__(bot, worker_manager, building_manager)
         # load all the column names and the data.
         self.model = model
-        self.game_info = bot.game_info
+        # self.game_info = bot.game_info
     
     def prepare_input(self):
         state = self.bot.state
 
-        print(self.game_info)
+        # print(self.game_info)
 
         observation = state.observation
 
         row = {}
         row["frame_id"] = observation.game_loop
 
+        row["resources"] = {
+        "minerals": observation.player_common.minerals,
+        "vespene": observation.player_common.vespene,
+        }
+
+        row["supply"] = {
+            "used": observation.player_common.food_used,
+            "total": observation.player_common.food_cap,
+            "army": observation.player_common.food_army,
+            "workers": observation.player_common.food_workers,
+        }
+
+        allied_units = [
+            unit for unit in observation.raw_data.units if unit.alliance == 1
+        ]
+
+        allied_unit_types = set([unit.unit_type for unit in allied_units])
+        
+        row["units"] = {
+            unit_type: len([
+                unit.tag for unit in allied_units if unit.unit_type == unit_type
+            ]) for unit_type in allied_unit_types
+        }
+
         # state_doc["supply"] = observation.player_common.food_used
         # print(observation.player_common.supply)
 
     async def run(self):
+        print(self.bot.units)
         # x = self.prepare_input()
         # action = self.model.evaluate(x)
 
-        if self.bot.supply_left < 2:
-            await self.worker_manager.build(UnitTypeId.SUPPLYDEPOT)
-        elif random.random() > 0.75:
-            print("ProductionManager: train SCV.")
-            await self.building_manager.train(UnitTypeId.SCV)
-        elif len(self.bot.units(UnitTypeId.BARRACKS)) == 0:
-            print("ProductionManager: build Barracks.")
-            await self.worker_manager.build(UnitTypeId.BARRACKS)
-        else:
-            print("ProductionManager: train Marine.")
-            await self.building_manager.train(UnitTypeId.MARINE)
+        
