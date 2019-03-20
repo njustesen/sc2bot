718b14a6acd3df080002f35fc2e1fa05bf33a19e
Miguel González <miguelgondu@gmail.com>
2019-03-20 17:27:05 +0100

    Adds mine micro without testing

--- sc2bot/managers/army/squad.py
+++ sc2bot/managers/army/squad.py
@@ -37,7 +37,7 @@ class Squad:
                 for unit in self.units:
                     self.unit_move(unit, self.target, self.order)
 
-    def unit_move(self, unit, target, type="attack"):
+    def unit_move(self, unit, target, order="attack"):
         closest_enemy_ground_unit = self.bot.known_enemy_units.not_flying.closest_to(
             unit) if self.bot.known_enemy_units.not_flying.exists else None
         closest_enemy_air_unit = self.bot.known_enemy_units.flying.closest_to(
@@ -59,12 +59,41 @@ class Squad:
 
         # Decide what to do
         if closest_enemy_unit is not None:
-            if type == "attack" or closest_enemy_unit.distance_to(unit.position) < range_own:
+            if order == "attack" or closest_enemy_unit.distance_to(unit.position) < range_own:
 
                 # Basic attack
                 self._basic_attack(unit, closest_enemy_unit)
 
+                # Micro for widowmines
+                '''
+                If we're close to the enemy, burrow down.
+                '''
+                if unit.type_id == UnitTypeId.WIDOWMINE:
+                    self.actions.append(unit(AbilityId.BURROWDOWN_WIDOWMINE))
+                    '''
+                    Move a little bit away from the mines (i.e. bait)
+                    (I guess all units are doing this in _basic_attack)
+                    I don't know if we should uncomment this:
+                    '''
+                    # for unit in self.units:
+                    #     self.actions.append(unit.move(self.bot.start_location))
+
             else:
+
+                '''
+                If a widowmine is buried outside of the base and we're defending,
+                then bring it back
+                '''
+                if unit.type_id == UnitTypeId.WIDOWMINE:
+                    if unit.is_burrowed():
+                        self.actions.append(unit(AbilityId.BURROWUP_WIDOWMINE))
+                        self.actions.append(unit.move(self.bot.start_location))
+                        '''
+                        Is there a way of referencing the natural ramp?, if so, we should
+                        move the widowmines there.
+
+                        TODO: Change the location to which we're moving the widowmines
+                        '''
 
                 if random.randint(0, len(self.units)) == 0:
 
@@ -86,11 +115,11 @@ class Squad:
                         self.actions.append(unit.move(target))
 
     def _basic_attack(self, unit, closest_enemy_unit):
-        range = unit.air_range if closest_enemy_unit.is_flying else unit.ground_range
+        _range = unit.air_range if closest_enemy_unit.is_flying else unit.ground_range
         distance = closest_enemy_unit.distance_to(unit)
         # print("Range=", range)
         # print("Distance=", distance)
-        if distance < range * 0.8 and closest_enemy_unit.distance_to(self.bot.start_location) > unit.distance_to(self.bot.start_location):
+        if distance < _range * 0.8 and closest_enemy_unit.distance_to(self.bot.start_location) > unit.distance_to(self.bot.start_location):
             #direction_away = closest_enemy_unit.position.direction_vector(unit.position)
             #length = direction_away.distance2_to(Point2((0, 0)))
             #unit_vector = Point2((direction_away.x / length, direction_away.y / length))
